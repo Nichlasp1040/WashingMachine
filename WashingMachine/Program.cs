634860c83541bb3040eb500a9563cbc9451af503
Nichlas Simon Vedel Poulsen <nich1840@zbc.dk>
2020-08-25 02:02:23 +0200

    Add project files.

---
+++ WashingMachine/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace WashingMachine
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WashingMachine washingPower = new WashingMachine();
+            washingPower.PowerButten();
+
+            WashingMachine TheDoor = new WashingMachine();
+
+            if (TheDoor.Door == false)
+            {
+                Console.WriteLine("1. Åben doren og leg besidt toj ind");
+                string temp = Console.ReadLine();
+                while (temp == "1")
+                {
+                    TheDoor.Door = true;
+                    Console.WriteLine("Besidt tøj blev lagt i vaske maskinen");
+
+                    Console.WriteLine("2. for at lukke døren");
+                    temp = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Velg program.\n1. Hurtig vask (30 min)\n2. Eco vask (1:30 time)\n3. Normal vask (1 time)");
+
+            while (washingPower.OnOrOff == true)
+            {
+                string userChooseProgram = Console.ReadLine();
+
+                switch (userChooseProgram)
+                {
+                    case "1":
+                        userChooseProgram = "Hurtig vask";
+                        break;
+                    case "2":
+                        userChooseProgram = "Eco vask";
+                        break;
+                    case "3":
+                        userChooseProgram = "Normal vask";
+                        break;
+                }
+
+                Console.WriteLine("velg temperatur\n1. 30 grader\n2. 40 grader\n3. 60 grader");
+
+                int userChooseTemperature = Convert.ToInt32(Console.ReadLine());
+                
+                switch (userChooseTemperature)
+                {
+                    case 1:
+                        userChooseTemperature = 30;
+                        break;
+                    case 2:
+                        userChooseTemperature = 40;
+                        break;
+                    case 3:
+                        userChooseTemperature = 60;
+                        break;
+                }
+
+                WashingMachine washingMachineProgram = new WashingMachine(userChooseProgram, userChooseTemperature);
+
+                Console.WriteLine(washingMachineProgram.Temperature + "\n" + washingMachineProgram.WashingProgram);
+
+            }
+
+        }
+    }
+}
---
+++ WashingMachine/WashingMachine.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WashingMachine
+{
+    class WashingMachine
+    {
+
+        #region Variables
+
+        private bool onOrOff;
+        private bool door;
+        private string washingProgram;
+        private int temperature;
+        private int washingPowder;
+
+        #endregion
+
+        public WashingMachine()
+        {
+
+        }
+
+        public bool OnOrOff
+        {
+            get { return this.onOrOff; }
+            set { this.onOrOff = false; }
+        }
+
+        public bool PowerButten()
+        {
+            if (onOrOff == false)
+            {
+                Console.WriteLine("Vaske maskinen er slukket");
+            }
+            else
+            {
+                Console.WriteLine("Vaske maskinen er tendt");
+            }
+
+            Console.WriteLine("1. Tend");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    onOrOff = true;
+                    break;
+                case "2":
+                    onOrOff = false;
+                    break;
+            }
+
+            return onOrOff;
+        }
+
+        public bool Door
+        {
+            get { return this.door; }
+            set { this.door = false; }
+        }
+
+        public int WashingPowder
+        {
+            get { return this.washingPowder; }
+            set { this.washingPowder = value; }
+        }
+
+        public string WashingProgram
+        {
+            get { return this.washingProgram; }
+            set
+            {
+                if (value == "Hurtig vask" || value == "Eco vask" || value == "Normal vask")
+                {
+                    this.washingProgram = value;
+                }
+            }
+        }
+
+        public int Temperature
+        {
+            get { return this.temperature; }
+            set
+            {
+                if (value == 30 || value == 40 || value == 60)
+                {
+                    this.temperature = value;
+                }
+            }
+        }
+
+        public WashingMachine(string chooseProgram, int ChooseTemperature)
+        {
+            WashingProgram = chooseProgram;
+            Temperature = ChooseTemperature;
+        }
+
+    }
+}
